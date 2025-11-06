52f3971eb8f979113ea1eb4e609b5f3b9362f6eb
pardeepcitm <63185204+pardeepcitm@users.noreply.github.com>
2025-11-06 22:35:53 +0100

    swagger for prod

--- Program.cs
+++ Program.cs
@@ -25,11 +25,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
